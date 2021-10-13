c0afe653d474ba739813fc58a90721bf56f46670
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2021-10-13 13:09:22 -0600

    Parses the cl arguments using clap

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,23 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Sam Ezebunandu <sam.ezebunandu@gmail.com>")
         .about("echo program written in Rust")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .help("Do not print newline")
+                .takes_value(false)
+                .short("n"),
+        )
         .get_matches();
+    println!("{:#?}", matches);
 }
